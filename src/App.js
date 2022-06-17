b0c8b2a443637cf783fd7101571b91580c78633e
mati <mati@gmail.com>
2022-06-17 15:17:40 -0300

    added shopping cart route

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Components
 import Navbar from "./components/sections/Navbar";
 import Footer from "./components/sections/Footer";
+import ShopingCart from "./components/pages/Cart";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Router>
         <Routes>
+        <Route path="/cart" element={<ShopingCart />} />
         </Routes>
       </Router>
       <Footer />
