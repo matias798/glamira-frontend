a3a1a5eff6748d4248dd396647b97fcbc8454feb
mati <mati@gmail.com>
2022-06-23 23:39:34 -0300

    Changes in Stay in touch
    When someone clicks the button an alert event starts

--- src/components/sections/StayInTouch.js
+++ src/components/sections/StayInTouch.js
@@ -1,26 +1,35 @@
+import Swal from "sweetalert2";
 const StayInTouch = () => {
   // stay in touch section
+const subscribeHandler = () => {
+  Swal.fire({
+    icon: "success",
+    text: "You have successfully subscribed",
+    showConfirmButton: false,
+    timer: 1500,
+  });}
 
   return (
-    <div className="my-5">
-      {/* title */}
-      <div className="title">
-        <h2>Stay in touch</h2>
-        <h4>Suscribe to our newsleter</h4>
+    <div className="container mt-5 pt-5">
+      <div className="my-5">
+        {/* title */}
+        <div className="title">
+          <h4>Suscribe to our newsleter</h4>
 
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control m-4"
-            placeholder="Enter your name"
-          />
-          <input
-            type="email"
-            className="form-control m-4"
-            placeholder="Enter your email"
-          />
-          <div className="d-flex justify-content-end">
-            <button className="btn btn-primary rounded">Subscribe</button>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control m-4"
+              placeholder="Enter your name"
+            />
+            <input
+              type="email"
+              className="form-control m-4"
+              placeholder="Enter your email"
+            />
+            <div className="d-flex justify-content-end">
+              <button className="btn btn-primary rounded" onClick={()=>{subscribeHandler()}}>Subscribe</button>
+            </div>
           </div>
         </div>
       </div>
