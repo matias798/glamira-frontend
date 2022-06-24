b5dfee469b30f6eb53dc3571100406640e7a6a24
mati <mati@gmail.com>
2022-06-24 00:02:59 -0300

    the detail of the product now displays the category an permits to direct to it

--- src/components/pages/ProductDetail.js
+++ src/components/pages/ProductDetail.js
@@ -2,12 +2,17 @@ import { useLocation } from "react-router";
 import { useNavigate } from "react-router-dom";
 import { useCart } from "react-use-cart";
 
+// components
+import ProductsSlider from "../sections/ProductsSlider";
+
 const ProductDetail = () => {
-  window.scrollTo(0, 0);
+  
+  window.scrollTo(0, 0); //always go to top of page
   const { addItem } = useCart();
   const navigate = useNavigate();
 
   let product = useLocation().state.product;
+  let products = useLocation().state.products;
 
   const addToCartHandler = (product) => {
     addItem(product);
@@ -21,10 +26,10 @@ const ProductDetail = () => {
           <img
             src={product.image}
             alt={product.title}
-            className="img-fluid"
+            className="img-fluid mx-auto d-block"
             style={{
               height: "400px",
-              width: "80%",
+              width: "400px",
               borderRadius: "10px",
             }}
           />
@@ -33,7 +38,22 @@ const ProductDetail = () => {
           <h3 className="ProdDetTitle">{product.title}</h3>
           <legend> ${product.price}</legend>
           <p className="my-5">{product.description}</p>
-          {console.log(product)}
+
+          <div class="down-content">
+            <div class="categories">
+              <h6>
+                Category:{" "}
+                <span  
+                className="btn-link"
+                onClick={()=>{
+                  navigate(`/products/${product.category}`)
+                }}>
+                    {product.category}
+                </span>
+              </h6>
+            </div>
+          </div>
+
           <div className="d-flex justify-content-start mt-5">
             <button
               className="btn btn-primary rounded "
@@ -45,6 +65,13 @@ const ProductDetail = () => {
             </button>
           </div>
         </div>
+      </div>
+      <div className="my-5 mt-4 pt-3">
+        <ProductsSlider
+          products={products}
+          navigate={navigate}
+          ProductSliderTitle={"You May Also Like"}
+        />
       </div>
     </div>
   );
