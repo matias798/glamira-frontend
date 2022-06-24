6b786bd949096968ee7c6f4b7cc2da680dec56d9
mati <mati@gmail.com>
2022-06-23 23:51:04 -0300

    created routes and added spinner on load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+// Modules
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 // Components
 import Navbar from "./components/sections/Navbar";
@@ -7,17 +9,38 @@ import Home from "./components/pages/Home";
 import ProductDetail from "./components/pages/ProductDetail";
 import Footer from "./components/sections/Footer";
 import Payment from "./components/pages/Payment";
+import ProductCategory from "./components/pages/ProductCategory";
+import Error from "./components/pages/Error";
+import Spinner from "./components/sections/Spinner";
+import ContactUs from "./components/pages/ContactUs";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 800);
+  }, []);
+
+  // Return -> Spinner if pages are loading
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <div className="App">
       <Router>
-      <Navbar />
+        <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/cart" element={<ShopingCart />} />
+          <Route path="/contact-us" element={<ContactUs />} />
           <Route path="/payment" element={<Payment />} />
           <Route path="/product/:detail" element={<ProductDetail />} />
+          <Route path="/products/:category" element={<ProductCategory />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </Router>
       <Footer />
