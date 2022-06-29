1168b5be5d33cb6c458bd7f8ed62e7653f56a484
mati <mati@gmail.com>
2022-06-29 12:44:02 -0300

    Added video Component

---
+++ src/components/sections/Video.js
@@ -0,0 +1,16 @@
+const Video=()=>{
+    return(
+        <div className="container mb-5 pb-5">
+      <h2 className="text-start  my-5  centerOnMobile">This is you </h2>
+
+        <div className="m-2">
+            <video
+            className="w-100 h-100"
+            src="https://res.cloudinary.com/glamira/video/upload/v1636690925/media/wysiwyg/new_landing_page/211104_Glamira_ThisIsYou_Longversion_ONLINE_v08-A_EN_THI_wo_searchbar.mp4"
+            controls
+            ></video>
+        </div>
+        </div>
+    )
+    }
+export default Video;
