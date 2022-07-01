69f0a7aa4f10a25194ebb7b2a4f3e654cad799ce
mati <mati@gmail.com>
2022-06-30 23:18:45 -0300

    imported new routes and made orders routes a private one

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ import Error from "./components/pages/Error";
 import Spinner from "./components/sections/Spinner";
 import ContactUs from "./components/pages/ContactUs";
 import Orders from "./components/pages/Orders";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import PrivateRoutes from "./components/pages/PrivateRoutes";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
@@ -35,13 +38,22 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+          {/* Public Routes */}
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<ShopingCart />} />
           <Route path="/contact-us" element={<ContactUs />} />
           <Route path="/payment" element={<Payment />} />
           <Route path="/product/detail" element={<ProductDetail />} />
           <Route path="/products/:category" element={<ProductCategory />} />
-          <Route path="/user/orders" element={<Orders />} />
+          <Route path="/user/login" element={<Login />} />
+          <Route path="/user/Register" element={<Register />} />
+
+          {/* Private routes */}
+          <Route path="/user/profile" element={<PrivateRoutes />}>
+            <Route path="/user/profile/orders" element={<Orders />} />
+          </Route>
+
+          {/* Error route */}
           <Route path="*" element={<Error />} />
         </Routes>
       </Router>
