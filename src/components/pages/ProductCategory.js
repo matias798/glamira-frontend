ef3ffd6a3522c2ad414ad21b5ef420994e54c6b1
mati <mati@gmail.com>
2022-07-02 16:59:49 -0300

    fixed wrong dependency import

--- src/components/pages/ProductCategory.js
+++ src/components/pages/ProductCategory.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
 const ProductCategory = () => {
 
