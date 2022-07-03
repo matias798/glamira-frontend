9d471aa718a81093823139a37b2ec0764e01aff3
mati <mati@gmail.com>
2022-07-02 21:42:39 -0300

    iff the user is already logged in, it can purchase
    separates the logged user from the other roles.

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -2,13 +2,36 @@ import CartItem from "./../sections/CartItem";
 import { Link } from "react-router-dom";
 import { useCart } from "react-use-cart";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Cart = () => {
   const { isEmpty, items, cartTotal } = useCart();
   const navigate = useNavigate();
 
+  const isLoggedIn = localStorage.getItem("token");
+
   const continueHandler = () => {
     navigate("/payment");
+  };
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+
+    // Data sent to the server
+    let data = {
+      total: `${cartTotal}`,
+      items,
+      UserToken: localStorage.getItem("token"),
+    };
+
+    // create purchase order
+    const payment = await axios.post(
+      `${process.env.REACT_APP_SERVER_URL}/payment`,
+      data
+    );
+
+    // redirect to other website
+    window.location.href =payment.data;
   };
 
   if (isEmpty)
@@ -33,15 +56,27 @@ const Cart = () => {
       {/* payment section */}
       <div className="d-flex justify-content-end m-5 align-content-center">
         <h5 className="m-4">Total: {cartTotal}</h5>
-        <button
-          variant="contained"
-          className="btn btn-outline-primary"
-          onClick={() => {
-            continueHandler();
-          }}
-        >
-          continue 
-        </button>
+        {isLoggedIn ? (
+          <button
+            variant="contained"
+            className="btn btn-outline-primary"
+            onClick={(e) => {
+              paymentHandler(e);
+            }}
+          >
+            Purchase
+          </button>
+        ) : (
+          <button
+            variant="contained"
+            className="btn btn-outline-primary"
+            onClick={() => {
+              continueHandler();
+            }}
+          >
+            continue
+          </button>
+        )}
       </div>
     </div>
   );
