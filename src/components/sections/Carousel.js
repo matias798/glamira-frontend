a5f29c7464148973fbc762463edc09fb98c105eb
mati <mati@gmail.com>
2022-08-10 14:01:36 -0300

    changed carousel to banner

--- src/components/sections/Carousel.js
+++ src/components/sections/Carousel.js
@@ -1,55 +1,46 @@
 import Carousel from "react-bootstrap/Carousel";
-import "./../../assets/css/App.css";
 import { Link } from "react-router-dom";
+import mainImage from "./../../assets/images/slider-bg.jpg";
 
 const HomeCarousel = () => {
   return (
-    <Carousel variant="dark">
-      <Carousel.Item className="CarrouselContainer">
-        <Link to="/products/jewelery">
-          <img
-            className="CarrouselImage"
-            src="https://images.unsplash.com/photo-1558769132-cb1aea458c5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h1 className="CarrouselTitle">women's clothing</h1>
-          </Carousel.Caption>
-        </Link>
-      </Carousel.Item>
-      <Carousel.Item className="CarrouselContainer">
-        <Link to="/products/jewelery">
-          <img
-            className="CarrouselImage"
-            src="https://images.unsplash.com/photo-1584302179602-e4c3d3fd629d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h1 className="CarrouselTitle">Jewelry</h1>
-          </Carousel.Caption>
-        </Link>
-      </Carousel.Item>
-      <Carousel.Item className="CarrouselContainer">
-        <Link to="/products/men's%20clothing">
-          <img
-            src="https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            alt="Second slide"
-            className="CarrouselImage"
-          />
-
-          <Carousel.Caption>
-            <h1
-              className="CarrouselTitle"
-              style={{ color: "#ffffff", textShadow: "2px 2px black " }}
-            >
-              Men's clothing
-            </h1>
-          </Carousel.Caption>
-        </Link>
-      </Carousel.Item>
-    </Carousel>
+    <section className="slider_section ">
+    <div className="slider_bg_box">
+       <img src={mainImage} alt=""/>
+    </div>
+    <div id="customCarousel1" className="carousel slide" data-ride="carousel">
+       <div className="carousel-inner">
+          <div className="carousel-item active">
+             <div className="container ">
+                <div className="row">
+                   <div className="col-md-7 col-lg-6 ">
+                      <div className="detail-box">
+                         <h1>
+                            <span>
+                            Sale 20% Off
+                            </span>
+                            <br/>
+                            On Everything
+                         </h1>
+                         <p>
+                            Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
+                         </p>
+                         <div >
+                            <Link to="/products/jewelery" className="btn1">
+                            Shop Now
+                            </Link>
+                            
+                         </div>
+                      </div>
+                   </div>
+                </div>
+             </div>
+          </div>
+    
+       </div>
+    
+    </div>
+ </section>
   );
 };
 export default HomeCarousel;
