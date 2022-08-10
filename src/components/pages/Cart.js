e20f2577fb1455409da4089b43ba33b02175556d
mati <mati@gmail.com>
2022-08-10 14:04:56 -0300

    added implementation of empty component and new swal
    if the user isnt loged in will pop a swal that redirects to login.
    implemented empty cart view

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -1,17 +1,30 @@
-import CartItem from "./../sections/CartItem";
-import { Link } from "react-router-dom";
+// Modules
 import { useCart } from "react-use-cart";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Swal from "sweetalert2";
+
+// Components
+import CartItem from "./../sections/CartItem";
+import EmptyCart from "./../sections/EmptyCart";
 
 const Cart = () => {
-  const { isEmpty, items, cartTotal } = useCart();
   const navigate = useNavigate();
-
+  const { isEmpty, items, cartTotal } = useCart();
   const isLoggedIn = localStorage.getItem("token");
 
   const continueHandler = () => {
-    navigate("/payment");
+    // tell the user that they need to login to checkout
+    Swal.fire({
+      icon: "info",
+      text: "Please login to checkout",
+      confirmButtonText: "OK",
+      confirmButtonColor: "#1a1a1a",
+    }).then((el) => {
+      if (el.isConfirmed === true) {
+        navigate("/user/login");
+      }
+    });
   };
 
   const paymentHandler = async (e) => {
@@ -30,23 +43,16 @@ const Cart = () => {
       data
     );
 
+    localStorage.removeItem("react-use-cart");
+
     // redirect to other website
-    window.location.href =payment.data;
+    window.location.href = payment.data;
   };
 
-  if (isEmpty)
-    return (
-      <div className="container my-5 py-5">
-        <div className="row">
-          <div className="col-md-6">
-            <h3>Your cart is empty</h3>
-            <button className="btn btn-outlined-primary">
-              <Link to="/">Continue Shopping</Link>
-            </button>
-          </div>
-        </div>
-      </div>
-    );
+  // if the cart is empty, display the empty cart component
+  if (isEmpty) return <EmptyCart />;
+
+  // if the cart is not empty, display the cart items
   return (
     <div className="m-5">
       {items.map((product) => (
@@ -56,6 +62,7 @@ const Cart = () => {
       {/* payment section */}
       <div className="d-flex justify-content-end m-5 align-content-center">
         <h5 className="m-4">Total: {cartTotal}</h5>
+
         {isLoggedIn ? (
           <button
             variant="contained"
